aba355a70fc0059cab62988d57af934ef846a8e6
Makstarr <maksi112/@>
2020-05-02 12:32:13 +0300

    Api added

---
+++ build/precache-manifest.ac192deb3523801cf06bca1e3649345d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0625df52584613ed8fa711763937b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4503559b906f93c81c77",
+    "url": "/static/css/main.0f2e9c10.chunk.css"
+  },
+  {
+    "revision": "e958a6b70ba5fee9c14c",
+    "url": "/static/js/2.41bc0408.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.41bc0408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4503559b906f93c81c77",
+    "url": "/static/js/main.2666efe0.chunk.js"
+  },
+  {
+    "revision": "2800fcddd66813d244bf",
+    "url": "/static/js/runtime-main.5db5293e.js"
+  },
+  {
+    "revision": "077e1d78d963a134bc28f33b05c457a6",
+    "url": "/static/media/close.077e1d78.svg"
+  },
+  {
+    "revision": "4e1807e121989d993a39553918f52fb6",
+    "url": "/static/media/do-it.4e1807e1.png"
+  },
+  {
+    "revision": "78ee146b9a3dcdee0856abd61b4d0970",
+    "url": "/static/media/edit.78ee146b.svg"
+  },
+  {
+    "revision": "b7ea4b700d29e489f607268120ad4b6a",
+    "url": "/static/media/list.b7ea4b70.svg"
+  },
+  {
+    "revision": "4d68f77aaec7be43ba9251e2bb4333a9",
+    "url": "/static/media/remove.4d68f77a.svg"
+  }
+]);
