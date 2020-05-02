169044154b1b9e407203dab0aec3c3ff8b268943
Makstarr <maksi112/@>
2020-05-02 12:46:25 +0300

    Api added

---
+++ src/components/AddList.jsx
@@ -0,0 +1,94 @@
+import React, {useState, useEffect}  from 'react'
+import axios from 'axios'
+import classNames from 'classnames'
+
+import List from './List'
+import Close from '../assets/img/close.svg'
+
+const AddList  = ({onAdd, colors}) =>{
+    const [popUp, showPopUp] = useState(false);
+    const [selectedColor, selectColor] = useState(1);
+    const [inputValue, setInputValue] = useState("");
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        if (colors){
+            selectColor(colors[0]["id"])
+        }
+    },[colors]);
+
+    const addList  = () =>{
+        if (!inputValue){
+            alert("A list without a name? Excuese me mister");
+            return;
+        }
+        setLoad(true);
+
+        let data = {
+            name:inputValue, 
+            colorId: selectedColor,
+            color: colors.find(color => color.id===selectedColor),
+            tasks:[]
+        };
+        axios.post('https://tojustdoit-api.herokuapp.com/lists-add', data)
+        .then(function(response){
+            console.log(response);
+        //Perform action based on response
+            })
+            .catch(function(error){
+                console.log(error);
+        //Perform action based on error
+            })
+        .finally(()=>{
+            onAdd(data);
+            showPopUp(!popUp);
+            setInputValue("");
+            selectColor(colors[0]["id"])
+            setLoad(false);
+        })
+    };
+
+    return <div  className= 'add-list'  >  
+        <List onClickAdd={() => showPopUp(!popUp)} items= {[
+            {
+            className: 'add-list__button',
+            icon: <svg width="15" height="15" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 1V15" stroke="#eeeeee" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M1 8H15" stroke="#eeeeee" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>,
+            name:'Add list'
+            }
+        ]}isRemovable={false}/>
+        {popUp && <div className="add-list__popup">
+                <button onClick={()=>showPopUp(false)} className="add-list__close">
+                    <i>
+                       {<img src={Close} alt="CLOSE POPUP"/>  }
+                    </i>
+                </button>
+                
+                <input 
+                className="field" 
+                type="text" 
+                maxLength = "250"
+                placeholder="Task name" 
+                value={inputValue}
+                onChange={(event)=>setInputValue(event.target.value)}/>
+
+                <ul className="add-list__colors">
+                    {
+                    colors.map((i) => (<li 
+                        onClick={()=>selectColor(i.id)} 
+                        key={i.id} 
+                        className= {classNames("add-list__circle", i.name, {'active':(i.id===selectedColor)})}>
+                        </li>)) 
+                    }
+                </ul>
+                <button onClick={addList} className="add-list__button button">{load ? 'Working...' : 'Add list'}</button>
+            </div>}
+    </div> 
+}
+
+export default AddList;
+
+
+
---
+++ src/components/List.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import classNames from 'classnames'
+import axios from 'axios'
+
+import removeBtn from '../assets/img/remove.svg'
+
+const List = ({items, isRemovable, onClickAdd, onClickItem, onRemove, activeItem}) =>{
+    const removeList = (i) => {
+        if (window.confirm('Are you sure you wanna delete this whole list?')){
+            let data = {id:i.id}
+            onRemove(i.id)
+
+
+            axios.post('https://tojustdoit-api.herokuapp.com/lists-delete', data)
+            .then(function(response){
+                console.log(response);
+            //Perform action based on response
+                })
+            .catch(()=>{
+                alert('Database error :((')
+            });
+        }
+    }
+    
+    return (
+        <ul onClick={onClickAdd} className="todo__list" >
+            {  
+                items?(items.map((i, index) => ( 
+                <li 
+                key={index} 
+                className={classNames(i.className, {'active': (activeItem&&activeItem.id===i.id)} )}
+                onClick={()=>onClickItem?onClickItem(i):"none"} 
+                >
+                {i.icon ?  <div className='i'>{i.icon}</div> : <div className={classNames('circle', i.color.name, 'i')}>{
+                i.tasks?(i.tasks.length>0?i.tasks.length:""):""
+                }</div>
+                }
+                <span>{
+                i.name
+                }</span>
+                {isRemovable ? <button className="list__remove-icon"  onClick={()=>removeList(i)}>  
+                                <img src={removeBtn} alt="Remove the list button"/>
+                               </button> :""} 
+                </li>
+                ))):""
+            }
+        </ul> 
+    );
+};
+
+export default List;
